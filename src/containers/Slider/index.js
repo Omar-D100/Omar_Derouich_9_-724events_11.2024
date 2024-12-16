15218e03a4141800bc06e7fdedf00dd7cc84620d
OmarZoldik <omar10.drh@outlook.fr>
2024-12-16 14:06:25 +0100

    modification file package

--- src/containers/Slider/index.js
+++ src/containers/Slider/index.js
@@ -8,14 +8,21 @@ const Slider = () => {
   const { data } = useData();
   const [index, setIndex] = useState(0);
   const byDateDesc = data?.focus.sort((evtA, evtB) =>
-    new Date(evtA.date) < new Date(evtB.date) ? -1 : 1
+     // Correction pour afficher du plus ancien au plus récent
+    new Date(evtA.date) < new Date(evtB.date) ? 1 : -1
   );
+  
+  // Correction pour que le slider affiche la première image une fois arrivé à la dernière sans afficher de page blanche entre les 2
+  const dataLength = data?.focus.length
+
   const nextCard = () => {
     setTimeout(
-      () => setIndex(index < byDateDesc.length ? index + 1 : 0),
+      // Ajout de -1 après "length"
+      () => setIndex(index < dataLength - 1 ? index + 1 : 0),
       5000
     );
   };
+
   useEffect(() => {
     nextCard();
   });
@@ -40,12 +47,18 @@ const Slider = () => {
           </div>
           <div className="SlideCard__paginationContainer">
             <div className="SlideCard__pagination">
-              {byDateDesc.map((_, radioIdx) => (
-                <input
-                  key={`${event.id}`}
-                  type="radio"
-                  name="radio-button"
-                  checked={idx === radioIdx}
+              {byDateDesc?.map((event, radioIdx) => (
+          // {byDateDesc.map((_, radioIdx)
+            <input
+              key={event.title}
+              // key={`${event.id}`} correction event n'a pas d'id
+              type="radio"
+              name="radio-button"
+              checked={index === radioIdx}
+              // checked={idx === radioIdx} correction idx => index 
+              readOnly
+              // Erreur de console : Avertissement : vous avez fourni un accessoire « checked » à un champ de formulaire sans gestionnaire « onChange ».
+              // Cela rendra un champ en lecture seule. Si le champ doit être mutable, utilisez « defaultChecked ». Sinon, définissez « onChange » ou « readOnly »
                 />
               ))}
             </div>
